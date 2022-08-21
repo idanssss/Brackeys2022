fa11949dd65d4098ec91f0a571d6bf870ce49a5f
DarknessRisesFromBelow <spam.me.please.do.it@gmail.com>
2022-08-21 23:06:56 +0300

    idiots

---
+++ Scripts/ConstantReferences.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantReferences : MonoBehaviour
+{
+    public List<GameObject> prefabs = new List<GameObject>();
+    public List<MonoBehaviour> scripts = new List<MonoBehaviour>();
+    public List<Texture2D> Textures = new List<Texture2D>();
+
+    public static ConstantReferences instantce = this; 
+}
--- Scripts/RoomManager.cs
+++ Scripts/RoomManager.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    public static void GenerateRoom(Vector2 pos, Vector2 size) => throw new System.NotImplementedException();
+    public static void GenerateRoom(Vector2 pos, Vector2 size)
+    {
+        for(int x = 0; x < size.x; x++)
+        {
+            for(int y = 0; y < size.y; y++)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
 }
